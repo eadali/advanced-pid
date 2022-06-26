deb82f875c278be6b905da79f626bf808df0f783
eadali <erkanadali91@gmail.com>
2022-06-26 22:28:27 +0300

    update reqs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,11 +38,13 @@ release = 'latest'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'm2r2',
+    'sphinx.ext.githubpages',
     'sphinxcontrib.napoleon',
+    'm2r2'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
